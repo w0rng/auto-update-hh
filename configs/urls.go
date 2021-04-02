cbfdb0631a8883858be34b5c2fb89f13fe0c5c6d
w0rng <antonabramov2000@gmail.com>
2021-04-02 18:58:57 +0700

    URL's для запрсоов

---
+++ configs/urls.go
@@ -0,0 +1,8 @@
+package configs
+
+const (
+	HH_URL            = "https://api.hh.ru/"
+	PUBLIS_RESUME_URL = HH_URL + "resumes/%s/publish"
+	MINE_RESUME_URL   = HH_URL + "resumes/mine"
+	AUTH_URL          = "https://hh.ru/oauth/token"
+)
